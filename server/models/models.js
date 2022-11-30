8a798ee2846b88518b56a5694d63587c1c439937
unknown <khangpro31072002@gmail.com>
2022-11-30 08:52:46 +0700

    Add database connection

--- server/models/models.js
+++ server/models/models.js
@@ -1,1 +1,14 @@
 // database-related methods here
+const mysql = require("mysql");
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "asgmt2"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
